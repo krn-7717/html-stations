72adb150ca68e78d61d2c5bc462e9aea9c48e33a
krn-7717 <104199555+krn-7717@users.noreply.github.com>
2023-11-20 01:45:37 +0900

    [finish]station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -2,7 +2,7 @@ async function getData() {
   // Promiseを使った実装をやってみましょう！APIとの通信でよく使う表現になります！
   // ３秒後にフルネームのリストを表示されるようにしましょう。
   // 最初から表示されていると、クリアになりません。
-  const userList = [
+  userList = [
     { id: 1, first_name: '優', family_name: '大木', affiliation: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affiliation: 'HogeHoge大学', is_student: true }
   ];
@@ -11,6 +11,14 @@ async function getData() {
 }
 
 function test() {
-  return
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(userList.map(buildFullName))
+    }, 3000)
+  })
 }
 
+function buildFullName(data) {
+  data.full_name = data.family_name + " " + data.first_name;
+  return data;
+}
